ccafad8f8480e5f23b0bbd4c7b2ed3281e71be19
austinckohler <austinckohler@gmail.com>
2020-06-17 13:14:25 -0600

    added update and destroy method to supplies

--- app/controllers/supplies_controller.rb
+++ app/controllers/supplies_controller.rb
@@ -17,10 +17,26 @@ class SuppliesController < ApplicationController
         render json: @supply
     end
 
+    def update 
+        @supply = Supply.find(supply_params)
+        render json: @supply
+    end
+    
+
+    def destroy 
+        @supply = Supply.find(params[:id])
+        if @supply.destroy
+            render status: 200
+        else 
+            render status: 422
+        end
+    end
+
     private
 
     def supply_params
         params.require(:supply).permit(:category, :item, :completed).merge(user_id: @user_id)
     end
+
 end
 
